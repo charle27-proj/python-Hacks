7d62f4f63776c559b16fab8463f5178c49cc3ae1
charle27-proj <bannurachakonda@gmail.com>
2026-02-13 18:11:25 +0530

    Update phone number in WhatsappMessage.py

--- WhatsappMessage.py
+++ WhatsappMessage.py
@@ -1,7 +1,7 @@
 import pywhatkit as kit
 
 # Phone number with country code 
-phone_number = "+1234567890"  # Replace with the recipient's phone number
+phone_number = "+91 9701874942"  # Replace with the recipient's phone number
 message = "Hello! This message is sent using Python � By code_with_asim"
 
 # Send instantly (opens WhatsApp Web)
